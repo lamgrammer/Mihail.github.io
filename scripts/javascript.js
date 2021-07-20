8f20c843f5a08f5b2d5259275652500491befd12
Mihail <78718334+lamgrammer@users.noreply.github.com>
2021-07-20 12:01:12 +0600

    Add files via upload

---
+++ scripts/javascript.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/firefox-icon.png') {
+      myImage.setAttribute ('src','img/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','img/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Имя?');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
